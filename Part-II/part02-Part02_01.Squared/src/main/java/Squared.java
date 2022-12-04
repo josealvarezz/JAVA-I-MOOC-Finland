a3e71e565ad5261c5352fe0b8a99b31c50de42f4
josealvarezz <jalvchi273@g.educaand.es>
2022-12-04 14:19:11 +0100

    Exercise 1 part II completed

--- Part-II/part02-Part02_01.Squared/src/main/java/Squared.java
+++ Part-II/part02-Part02_01.Squared/src/main/java/Squared.java
@@ -6,5 +6,7 @@ public class Squared {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int number = Integer.valueOf(scanner.nextLine());
+        System.out.println(number * number);
     }
 }
