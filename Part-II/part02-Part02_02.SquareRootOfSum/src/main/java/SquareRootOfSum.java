d2a0a78d2abcd2d88724be383204cc5a8b2cbcae
josealvarezz <jalvchi273@g.educaand.es>
2022-12-05 22:00:00 +0100

    Exercise 2 part II completed

--- Part-II/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ Part-II/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -6,5 +6,12 @@ public class SquareRootOfSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        int sum = first + second;
+        double squareRoot = Math.sqrt(sum);
+        System.out.println(squareRoot);
     }
 }
