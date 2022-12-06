a89903835bf6cf17587a2ede6433ed59f8f0dfb0
josealvarezz <jalvchi273@g.educaand.es>
2022-12-06 13:23:02 +0100

    Exercise 5 part II completed

--- Part-II/part02-Part02_05.CarryOn/src/main/java/CarryOn.java
+++ Part-II/part02-Part02_05.CarryOn/src/main/java/CarryOn.java
@@ -6,5 +6,13 @@ public class CarryOn {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String input;
+        while (true) {
+            System.out.println("Shall we carry on?");
+            input = scanner.nextLine().toUpperCase();
+            if (input.equals("NO")) {
+                break;
+            }
+        }
     }
 }
