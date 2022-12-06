3748d9c19f9f5893671ee81ae75745e499b2bfbf
josealvarezz <jalvchi273@g.educaand.es>
2022-12-06 12:45:32 +0100

    Exercise 3 part II completed

--- Part-II/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ Part-II/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -5,6 +5,14 @@ public class AbsoluteValue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+     
+        System.out.println("Give a number:");
+        int number = Integer.valueOf(scanner.nextLine());
+        if (number < 0) {
+            int absolute = number * -1;
+            System.out.println("The absolute number is " + absolute);
+        } else {
+            System.out.println(number);
+        }
     }
 }
