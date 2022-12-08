e33431523e6089be348ae45bc105aa9f66aa6bf7
josealvarezz <jalvchi273@g.educaand.es>
2022-12-08 18:12:53 +0100

    Exercise 15 part II completed

--- Part-II/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ Part-II/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -6,5 +6,10 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int number = Integer.valueOf(scanner.nextLine());
+        while (number <= 100) {
+            System.out.println(number);
+            number++;
+        }
     }
 }
