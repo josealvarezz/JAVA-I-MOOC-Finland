9ab7f372c6b15b1abdb4631de84267490376b30c
josealvarezz <jalvchi273@g.educaand.es>
2022-12-08 18:35:20 +0100

    Exercise 16 part II completed

--- Part-II/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ Part-II/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -7,5 +7,12 @@ public class FromWhereToWhere {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Where to?");
+        int limit = Integer.valueOf(scanner.nextLine());
+        System.out.println("Where from?");
+        int origin = Integer.valueOf(scanner.nextLine());
+        for (int i = origin; i <= limit; i++) {
+            System.out.println(i);
+        }
     }
 }
