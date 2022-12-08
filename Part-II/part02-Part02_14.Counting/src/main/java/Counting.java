fe3fad9b04149431dd9bfae5390bf7e1a23b2083
josealvarezz <jalvchi273@g.educaand.es>
2022-12-08 18:09:57 +0100

    Exercise 14 part II completed

--- Part-II/part02-Part02_14.Counting/src/main/java/Counting.java
+++ Part-II/part02-Part02_14.Counting/src/main/java/Counting.java
@@ -6,5 +6,9 @@ public class Counting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int limit = Integer.valueOf(scanner.nextLine());
+        for (int i = 0 ; i <= limit ; i++) {
+            System.out.println(i);
+        }
     }
 }
