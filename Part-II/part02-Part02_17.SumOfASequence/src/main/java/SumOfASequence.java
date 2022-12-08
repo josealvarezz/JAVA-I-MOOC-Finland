b2f208459f53fe2be7bd9ccafe95c2fb130e02ca
josealvarezz <jalvchi273@g.educaand.es>
2022-12-08 19:10:09 +0100

    Exercise 17 part 2 completed

--- Part-II/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ Part-II/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -6,5 +6,13 @@ public class SumOfASequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Last number?");
+        int last = Integer.valueOf(scanner.nextLine());
+        int numbersum = 0;
+        
+        for (int i = 0 ; i <= last ; i++){
+            numbersum += i;
+        }
+        System.out.println("The sum is " + numbersum);
     }
 }
