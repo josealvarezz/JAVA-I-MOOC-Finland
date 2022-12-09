3c156a87ab0a591f5e3ce73f2b500ffe60f6b928
josealvarezz <jalvchi273@g.educaand.es>
2022-12-09 19:31:50 +0100

    Exercise 33 part II completed

--- Part-II/part02-Part02_33.StarSign/src/main/java/StarSign.java
+++ Part-II/part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -12,7 +12,7 @@ public class StarSign {
         System.out.println("\n---");  // printing --- between the shapes
         printSquare(4);
         System.out.println("\n---");
-        printRectangle(5, 6);
+        printRectangle(17, 3);
         System.out.println("\n---");
         printTriangle(3);
         System.out.println("\n---");
@@ -20,17 +20,30 @@ public class StarSign {
 
     public static void printStars(int number) {
         // first part of the exercise
+        for (int i = 1; i <= number; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
     }
 
     public static void printSquare(int size) {
         // second part of the exercise
+        for (int i = 1; i <= size; i++) {
+            printStars(size);
+        }
     }
 
     public static void printRectangle(int width, int height) {
         // third part of the exercise
+        for (int i = 1; i <= height; i++) {
+            printStars(width);
+        }
     }
 
     public static void printTriangle(int size) {
         // fourth part of the exercise
+        for (int i = 1; i <= size; i++) {
+            printStars(i);
+        }
     }
 }
