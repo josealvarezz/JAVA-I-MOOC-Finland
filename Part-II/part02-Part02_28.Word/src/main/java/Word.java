412477f5afb4b5062702adb2a9ca88e720553fa2
josealvarezz <jalvchi273@g.educaand.es>
2022-12-09 14:13:00 +0100

    Exercise 28 part II completed

--- Part-II/part02-Part02_28.Word/src/main/java/Word.java
+++ Part-II/part02-Part02_28.Word/src/main/java/Word.java
@@ -6,6 +6,10 @@ public class Word {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        word();
     }
 
+    public static String word(){
+        return "Hi";
+    }
 }
