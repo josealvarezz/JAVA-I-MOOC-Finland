f5f4def36127a85b524611b5ff3ebe380f078b0d
josealvarezz <jalvchi273@g.educaand.es>
2022-12-11 14:00:11 +0100

    Exercise 12 part III completed

---
+++ Part-III/part03-Part03_12.SumOfAList/src/main/java/SumOfAList.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SumOfAList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        System.out.println("");
+
+        // toteuta listan lukujen summan laskeminen tänne
+        for (int i = 0; i < list.size(); i++) {
+            int number = list.get(i);
+            sum += number;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
