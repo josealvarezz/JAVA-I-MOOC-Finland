6c88154efc04f323a4e09bb4c45f9a4a42b00205
josealvarezz <jalvchi273@g.educaand.es>
2022-12-12 21:21:23 +0100

    Add Part IV directory along with the first exercise

---
+++ Part-IV/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,11 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account Account = new Account("Test account", 100.00);
+        Account.deposit(20.0);
+        System.out.println(Account);
+    }
+}
