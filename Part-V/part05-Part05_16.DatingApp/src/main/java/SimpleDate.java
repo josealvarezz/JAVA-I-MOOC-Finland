41e6eb91d545514fdf1fe6b36a9ec22213a7b117
josealvarezz <jalvchi273@g.educaand.es>
2023-03-11 14:45:05 +0100

    Exercise 16 part V completed

---
+++ Part-V/part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
@@ -0,0 +1,65 @@
+
+public class SimpleDate {
+
+    private int day;
+    private int month;
+    private int year;
+
+    public SimpleDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return this.day + "." + this.month + "." + this.year;
+    }
+
+    public boolean before(SimpleDate compared) {
+        if (this.year < compared.year) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month < compared.month) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month == compared.month
+                && this.day < compared.day) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void advance() {
+        this.day += 1;
+        if (this.day > 30) {
+            this.month += 1;
+            this.day = 1;
+        }
+
+        if (this.month > 12) {
+            this.month = 1;
+            this.year += 1;
+        }
+    }
+
+    public void advance(int howManyDays) {
+        for (int i = 0; i < howManyDays; i++) {
+            advance();
+        }
+    }
+
+    public SimpleDate afterNumberOfDays(int days) {
+        SimpleDate newDate = new SimpleDate(this.day, this.month, this.year);
+
+        // Add the specified number of days to the date
+        for (int i = 0; i < days; i++) {
+            newDate.advance();
+        }
+
+        return newDate;
+    }
+}
