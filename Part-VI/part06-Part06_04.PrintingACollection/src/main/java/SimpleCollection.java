787d9a4606159b932b2c84626a478c01dd31047a
josealvarezz <jalvchi273@g.educaand.es>
2023-04-08 14:54:49 +0200

    Exercise 4 part VI completed

---
+++ Part-VI/part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -0,0 +1,52 @@
+
+import java.util.ArrayList;
+
+public class SimpleCollection {
+
+    private String name;
+    private ArrayList<String> elements;
+
+    public SimpleCollection(String name) {
+        this.name = name;
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(String element) {
+        this.elements.add(element);
+    }
+
+    public ArrayList<String> getElements() {
+        return this.elements;
+    }
+    
+    
+    public String toString() {
+
+        String printString = "The collection " + this.name;
+
+        if (this.elements.isEmpty()) {
+            return printString + " is empty.";
+        }
+
+        String printValues = "";
+
+        for (String e : elements) {
+
+            if (elements.size() <= 1) {
+                printValues = e;
+            } else {
+                printValues += e + "\n";
+            }
+        }
+
+        String elementString = "";
+
+        if (elements.size() > 1) {
+            elementString = "elements:\n";
+        } else {
+            elementString = "element:\n";
+        }
+        return printString + " has " + elements.size() + " " + elementString + printValues;
+    }
+
+}
