95501a4812bfff140ff747e3b6d8f6e77ac8b422
josealvarezz <jalvchi273@g.educaand.es>
2023-05-08 11:45:14 +0200

    Exercise 8 part VIII completed

---
+++ Part-VIII/part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -0,0 +1,46 @@
+
+import java.util.HashMap;
+
+public class Program {
+
+    public static void main(String[] args) {
+        // Test your program here!
+        HashMap<String, String> hashmap = new HashMap<>();
+        hashmap.put("f.e", "for example");
+        hashmap.put("etc.", "and so on");
+        hashmap.put("i.e", "more precisely");
+
+        printKeys(hashmap);
+        System.out.println("---");
+        printKeysWhere(hashmap, "i");
+        System.out.println("---");
+        printValuesOfKeysWhere(hashmap, ".e");
+    }
+
+    public static void printKeys(HashMap<String, String> hashmap) {
+        String result = hashmap.keySet().toString();
+        
+        String[] listaResultados = result.split(",");
+        
+        for (String linea: listaResultados) {
+            System.out.println(linea);
+        }
+    }
+
+    public static void printKeysWhere(HashMap<String, String> hashmap, String text) {
+        for (String key : hashmap.keySet()) {
+            if (key.contains(text)) {
+                System.out.println(key);
+            }
+        }
+    }
+
+    public static void printValuesOfKeysWhere(HashMap<String, String> hashmap, String text) {
+        for (String key : hashmap.keySet()) {
+            if (key.contains(text)) {
+                System.out.println(hashmap.get(key));
+            }
+        }
+    }
+
+}
